fb49d86838e910cbed83f098bc00a495c0c33582
Vamsi Priya <vamsi.bantumilli@gmail.com>
2025-04-01 03:55:59 +0530

    March 6th task program

--- src/ex_01_Java_Basic.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ex_01_Java_Basic {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        }
-    }
