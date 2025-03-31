ae4c86a8dc46b45e3a2a5426097f8e655e7e9c73
Vamsi Priya <vamsi.bantumilli@gmail.com>
2025-03-31 12:43:03 +0530

    new commit hello world

--- src/ex_01_Java_Basic.java
+++ src/ex_01_Java_Basic.java
@@ -2,9 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ex_01_Java_Basic {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
         }
     }
