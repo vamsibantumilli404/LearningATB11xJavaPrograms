43fc662f04b3fae3d94e8179011f67422dc87ec0
Vamsi Priya <vamsi.bantumilli@gmail.com>
2025-03-31 12:32:15 +0530

    git commit

---
+++ src/ex_01_Java_Basic.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ex_01_Java_Basic {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        System.out.println("Hello World");
+        }
+    }
