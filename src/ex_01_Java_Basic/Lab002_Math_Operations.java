2fdc6f55bca6e39eafda512f4186d655a41992ab
Vamsi Priya <vamsi.bantumilli@gmail.com>
2025-04-01 14:33:27 +0530

    March 10th Math Operations and Exception

---
+++ src/ex_01_Java_Basic/Lab002_Math_Operations.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Math_Operations {
+    public static void main(String[] args) {
+        System.out.println(4+2);
+        System.out.println(5-9);
+        System.out.println(9*7);
+        System.out.println(9/4);
+        System.out.println(6/3);
+        System.out.println(8%2);
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab_002_Math_Exception.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab_002_Math_Exception {
+    public static void main(String[] args) {
+        System.out.println(8/0);
+    }
+}
