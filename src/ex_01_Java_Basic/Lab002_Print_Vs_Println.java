89e607f60c9c5663b5aa2aff090b7b41b84d1174
Vamsi Priya <vamsi.bantumilli@gmail.com>
2025-04-01 14:25:28 +0530

    March 10th print vs println

---
+++ src/ex_01_Java_Basic/Lab002_Print_Vs_Println.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Print_Vs_Println {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("Vamsi");
+        System.out.print("hi ");
+        System.out.print("vamsi");
+    }
+}
