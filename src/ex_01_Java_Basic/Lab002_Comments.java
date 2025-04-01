dd17f7f6d05a887f054aa5756e6ee62ea4ae50fc
Vamsi Priya <vamsi.bantumilli@gmail.com>
2025-04-01 12:28:06 +0530

    March 10th comments

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+//single line comment
+       // System.out.println("Hello");
+        /*multi line
+        comment
+         */
+        /*System.out.println("hi");
+        System.out.println("hurray!");*/
+    }
+}
